c12c3644247021b81afd967dfcfde9a8ab95c34b
Thinh T <duythinht@gmail.com>
2013-10-15 04:07:52 +0700

    OKIE

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello world!")
+
+application = tornado.web.Application([
+    ("/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(5000)
+    tornado.ioloop.IOLoop.instance().start()
