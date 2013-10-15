a2cd0dd04220e2ee2a83121e95b4c649a9d9b87e
Thinh T <duythinht@gmail.com>
2013-10-15 10:44:53 +0700

    Change to HttpServer

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
+import os
 import tornado.web
+import tornado.httpserver
+from tornado.options import define
+define("port", default=5000, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -11,5 +15,9 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
-    application.listen(8000)
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(os.environ.get("PORT", 5000))
+
+    # start it up
     tornado.ioloop.IOLoop.instance().start()
