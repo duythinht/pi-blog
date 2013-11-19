aee8326a11ee52f6016919291f286bfd4423b7ed
Thinh T <duythinht@gmail.com>
2013-11-19 08:56:40 +0700

    Fix DBCFG

--- duythinht/settings.py
+++ duythinht/settings.py
@@ -65,6 +65,7 @@ WSGI_APPLICATION = 'duythinht.wsgi.application'
 # }
 
 import dj_database_url
+DATABASES = []
 DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
