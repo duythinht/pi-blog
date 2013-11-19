0fbe94693ac82a68e6c4e47646f9e8f6d0435055
Thinh T <duythinht@gmail.com>
2013-11-19 08:49:43 +0700

    Add Disqus

--- duythinht/settings.py
+++ duythinht/settings.py
@@ -57,15 +57,15 @@ WSGI_APPLICATION = 'duythinht.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+# }
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
